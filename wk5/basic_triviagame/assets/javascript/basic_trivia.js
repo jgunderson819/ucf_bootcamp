aed08cb157846eb53bb06da656d809f57f4f73a6
Robert Finkley <rfinkley82@gmail.com>
2020-01-18 09:11:41 -0500

    Added basic clock functions

--- wk5/basic_triviagame/assets/javascript/basic_trivia.js
+++ wk5/basic_triviagame/assets/javascript/basic_trivia.js
@@ -1,14 +1,50 @@
-let s = 0;
-let m = 0;
+let time = 30;
+let clockRunning = false;
+let intervalID;
+let converted = timeConverter(time);
 
-do {
-    if (s < 60) {
-        s++;
-    } else {
-        m++;
-        s = 0;
+$(".timeRemaining").text("00:30");
+
+function start() {
+    if (!clockRunning) {
+        intervalID = setInterval(count, 1000);
+        clockRunning = true;
     }
-} while (m<60);
+}
 
+function stop() {
+    clearInterval(intervalID);
+    clockRunning = false;
+}
 
-setInterval(function () {});
+function timeConverter(t) {
+
+    var minutes = Math.floor(t / 60);
+    var seconds = t - (minutes * 60);
+
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    }
+
+    if (minutes === 0) {
+        minutes = "00";
+    } else if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+
+    return minutes + ":" + seconds;
+}
+
+function reset() {
+    time = 30;
+    $(".timeRemaining").text("00:30");
+}
+
+function count() {
+    time--;
+    let converted = timeConverter(time);
+    $(".timeRemaining").text(converted);
+    if (time==0) {
+        stop(intervalID);
+    }
+}
