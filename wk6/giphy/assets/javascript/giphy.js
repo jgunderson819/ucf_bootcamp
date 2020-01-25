28ac31618c2106569219060d4f76460bb8a0ff10
Robert Finkley <rfinkley82@gmail.com>
2020-01-25 16:34:52 -0500

    Add a button to HTML for every value in the animals array

--- wk6/giphy/assets/javascript/giphy.js
+++ wk6/giphy/assets/javascript/giphy.js
@@ -3,3 +3,20 @@ let animals = [
     "horse",
     "porcupine"
 ];
+// Add a button to HTML for every value in the animals array
+function addButtons() {
+    for (let i = 0; i < animals.length; i++) {
+        let button = $("<button>");
+        button.addClass("button");
+        button.attr({
+            "name": `${animals[i]}`,
+            "type": "submit",
+            "value": `${animals[i]}`
+        });
+        button.text(`${animals[i]}`);
+        $(".giphyButtons").append(button);
+    }
+}
+
+// Call the addButtons function
+addButtons();
