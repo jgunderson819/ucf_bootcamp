4723190c0a400808d00cbde21012f15fd2c625ec
Robert Finkley <rfinkley82@gmail.com>
2020-01-28 11:55:49 -0500

    removed reference to api-key

--- wk7/homework/rps/assets/js/fb_rps.js
+++ wk7/homework/rps/assets/js/fb_rps.js
@@ -1,18 +1,4 @@
-        // Firebase configuration
-        var firebaseConfig = {
-            apiKey: "AIzaSyCckmEbSfVqM3x89V3uP4BtGsnghy_PrWo",
-            authDomain: "rock-paper-scissors-fc33f.firebaseapp.com",
-            databaseURL: "https://rock-paper-scissors-fc33f.firebaseio.com",
-            projectId: "rock-paper-scissors-fc33f",
-            storageBucket: "rock-paper-scissors-fc33f.appspot.com",
-            messagingSenderId: "701121955513",
-            appId: "1:701121955513:web:7f3a6523e7bbec2ee2bd68",
-            measurementId: "G-1QPD5XZDWY"
-        };
-        // Initialize Firebase
-        firebase.initializeApp(firebaseConfig);
-        // firebase.analytics();
-        // Create variable to store fire
+
         let database = firebase.database();
 
         // Create varible to call connections to database
@@ -33,4 +19,4 @@
         //Display number of connections in connectedUsers
         connectedUsers.on("value", function (snap) {
             console.log(snap.numChildren());
-        });
+        });S
