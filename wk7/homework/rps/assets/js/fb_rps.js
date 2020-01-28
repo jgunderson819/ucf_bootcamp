20e3928b64f57d34dd43102eb1bbb784b90fe253
Robert Finkley <rfinkley82@gmail.com>
2020-01-27 19:11:42 -0500

    Added connection tracking and connection displaying

--- wk7/homework/rps/assets/js/fb_rps.js
+++ wk7/homework/rps/assets/js/fb_rps.js
@@ -20,6 +20,7 @@
         // Create snapshot of connectedRef to track connections
         let connectedUsers = database.ref('/connections');
 
+        //Added entry into connected users for every connection logged in .info/connected
         connectedRef.on("value", function (snap) {
                 if (snap.val()) {
                     connectedUsers.push(true).onDisconnect().remove();
@@ -28,3 +29,8 @@
             function (errorObject) {
                 console.log("The read failed: " + errorObject.code);
             });
+
+        //Display number of connections in connectedUsers
+        connectedUsers.on("value", function (snap) {
+            console.log(snap.numChildren());
+        });
