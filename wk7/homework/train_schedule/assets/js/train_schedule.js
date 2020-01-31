5c76a56620f634cb81ba78ff414e626fa0963ba1
Robert Finkley <rfinkley82@gmail.com>
2020-01-31 06:39:57 -0500

    Added code to check connections to Firebase

--- wk7/homework/train_schedule/assets/js/train_schedule.js
+++ wk7/homework/train_schedule/assets/js/train_schedule.js
@@ -1,3 +1,25 @@
+let database = firebase.database();
+
+// Create varible to call connections to database
+let connectedRef = database.ref(".info/connected");
+// Create snapshot of connectedRef to track connections
+let connectedUsers = database.ref('/connections');
+
+
+
+//Added entry into connected users for every connection logged in .info/connected
+connectedRef.on("value", function (snap) {
+        if (snap.val()) {
+            connectedUsers.push(true).onDisconnect().remove();
+        }
+    },
+    function (errorObject) {
+        console.log("The read failed: " + errorObject.code);
+    });
+
+
+
+
 // Assume the following situations.
 
 // (TEST 1)
